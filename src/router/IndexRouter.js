5740fae1cde0bc0afbe5f1f4faec14a0f0b2a72a
cpx <roo@cpx.local>
2022-07-02 10:34:22 +0800

    fix: while  rendering , Each child in a list should have a unique 'key' prop.

--- src/router/IndexRouter.js
+++ src/router/IndexRouter.js
@@ -9,7 +9,8 @@ export default function IndexRouter() {
         <Route path='/login' component={Login}></Route>
         <Route path='/' render={() => [
           //  对于未授权的请求路径，重定向到/login界面,否则放行
-          localStorage.getItem('token')? <NewsSandBox/>:<Redirect to="/login"/>
+          // 必须设置一个key，不然会有报错,此处传到是一个空对象{}
+          localStorage.getItem('token')? <NewsSandBox key={{}}/>:<Redirect to="/login"/>
         ]}></Route>
       </Switch>
     </Router>
